47c047a870f57f68075343109eaea7088c309307
Ranveer <udaykashyap.hjp@gmail.com>
2023-08-05 22:53:27 +0530

    configure the setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Button, Heading } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box
+      className="App"
+      color={"white"}
+      backgroundColor="blackAlpha.900"
+      height={"100vh"}
+    >
+      {/* Header Start here */}
+
+      <Heading textAlign={"center"}>Invoice</Heading>
+      <hr />
+      <Box display={"flex"} padding={"1rem"} justifyContent={"space-evenly"}>
+        <Button backgroundColor={"coral"}>Print</Button>
+        <Button backgroundColor={"coral"}>Download</Button>
+        <Button backgroundColor={"coral"}>Send</Button>
+      </Box>
+      <hr />
+      {/* Header End here */}
+    </Box>
   );
 }
 
