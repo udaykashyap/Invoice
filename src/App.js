db7d56c494074d262fb7fcb756a2f2c54d74f0a2
Ranveer <udaykashyap.hjp@gmail.com>
2023-08-07 22:54:26 +0530

    working on css front page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import { Box, Button, Heading } from "@chakra-ui/react";
+import { Box, Button, Heading, Image, Text } from "@chakra-ui/react";
+import Logo from "./Assets/Shop_Logo.png";
 
 function App() {
+  const currentDate = new Date().toLocaleDateString();
   return (
     <Box
       className="App"
-      color={"white"}
-      backgroundColor="blackAlpha.900"
+      // color={"white"}
+      // backgroundColor="blackAlpha.900"
+      padding={"3%"}
       height={"100vh"}
     >
       {/* Header Start here */}
-
-      <Heading textAlign={"center"}>Invoice</Heading>
-      <hr />
-      <Box display={"flex"} padding={"1rem"} justifyContent={"space-evenly"}>
-        <Button backgroundColor={"coral"}>Print</Button>
-        <Button backgroundColor={"coral"}>Download</Button>
-        <Button backgroundColor={"coral"}>Send</Button>
+      <Box lineHeight={"10"}>
+        <Image src={Logo} margin={"auto"} width={"120px"} />
+        <Text color={"#912008"} fontSize={"5xl"} className="cinzel-regular">
+          GAHNA JEWELLERS
+        </Text>
+        <Text fontWeight={"semibold"} className="cinzel-regular">
+          100% Hallmark Jeweller Showroom
+        </Text>
+        <hr />
       </Box>
-      <hr />
       {/* Header End here */}
+
+      {/* Customer's Details */}
+      <Box
+        display={"flex"}
+        justifyContent={"space-between"}
+        padding={"1%"}
+      >
+        <Box>
+          <Text>Name : Abhishek</Text>
+          <Text>Address : Patna</Text>
+        </Box>
+        <Box>
+          <Text fontSize={"2xl"} fontWeight={500}>
+            Invoice
+          </Text>
+          <Text>Invoce No : 001</Text>
+          <Text>Date : {currentDate}</Text>
+        </Box>
+      </Box>
+      {/* Customer's Details end */}
+
+      {/* Table */}
+      {/* Table end */}
+      {/* Notes */}
+
+      {/* Notes end */}
+      {/* Footer */}
+      <Box borderTop={"3px solid #912008"} textAlign={"center"}>
+        <Text><i class="fa-solid fa-location-dot" style={{ color: "#ff0000" }}></i> Anwarpur Chowk, Hajipur, Vaishali - 844101, Bihar,India</Text>
+        <Text><i class="fa-solid fa-envelope" style={{ color: "#0033ff" }}></i> gahna.hjp@gmail.com <i class="fa-brands fa-whatsapp" style={{ color: "#0cb800" }}></i> 7323057831 <i class="fa-solid fa-phone" style={{ color: "#073b08" }}></i> +91 9334057831, +91 7903646960</Text>
+      </Box>
+      {/* Footer end*/}
     </Box>
   );
 }
